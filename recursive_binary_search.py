dbb4e7dfcaebc739c776a985f6c1e2c1d670f9cd
Victoria045 <victoriaberyl45@gmail.com>
2022-07-22 13:55:43 +0300

    recursive binary search implementation

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -1,30 +1,24 @@
-# def recursive_binary_search(list, target):
-#   if len(list) == 0:
-#     return False
-#   else:
-#     midpoint = len(list) // 2
+def recursive_binary_search(list, target):
+  if len(list) == 0:
+    return False
+  else:
+    midpoint = len(list) // 2
 
-#     if list[midpoint] == target:
-#       return True
-#     else:
-#       if list[midpoint] < target:
-#         return recursive_binary_search(list[midpoint + 1:], target)
-#       else:
-#         return recursive_binary_search(list[:midpoint], target)
+    if list[midpoint] == target:
+      return True
+    else:
+      if list[midpoint] < target:
+        return recursive_binary_search(list[midpoint + 1:], target)
+      else:
+        return recursive_binary_search(list[:midpoint], target)
 
-# def verify(result):
-#   print("Target found: ", result)
+def verify(result):
+  print("Target found: ", result)
 
-# numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1,2,3,4,5,6,7,8,9,10]
 
-# result = recursive_binary_search(numbers, 20)
-# verify(result)
+result = recursive_binary_search(numbers, 20)
+verify(result)
 
-# result = recursive_binary_search(numbers, 9)
-# verify(result)
-
-nums = [1,3,4,5,6]
-
-sorted_nums = nums.sort()
-
-print(sorted_nums)
+result = recursive_binary_search(numbers, 9)
+verify(result)
